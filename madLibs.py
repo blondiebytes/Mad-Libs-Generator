d39bb1da24d5521247de27c570afe21a6aa6012d
Kathryn Hodge <kahodge@vassar.edu>
2015-01-22 11:54:26 -0600

    Adding to the story

--- madLibs.py
+++ madLibs.py
@@ -12,23 +12,28 @@ import os
 # If the user does not input anything (aka just presses enter), then the program will come up with a random
 # adjective, noun, etc to put in the space.
 
+# Add a feature where the user could create a story?
+
 class Story :
     __orderOfParts = [""]
-    __inputsEntered = [""]
-    __story = [""]
+    __inputsEntered = None
+    __text = None
 
-    def __init__ (self, orderOfParts, inputsEntered):
+    def __init__ (self, orderOfParts, story):
         self.__orderOfParts = orderOfParts
-        self.__inputsEntered = inputsEntered
+        self.__story = story
 
     def getOrderOfParts(self, specificIndex = None):
-        if specificIndex is None :
+        if specificIndex is None:
             return self.__orderOfParts
         else:
             return self.__orderOfParts[specificIndex]
 
+    def getBasicStoryText(self):
+        return self.__text
+
     def getInputsEntered(self, specificIndex = None):
-        if specificIndex is None :
+        if specificIndex is None:
             return self.__inputsEntered
         else:
             return self.__inputsEntered[specificIndex]
@@ -41,8 +46,28 @@ class Story :
             return "The story isn't ready yet!"
         else :
             #print out the story with the appropriate input
-            return ""
+            return self.putStoryTogether()
 
+    def putStoryTogether(self):
+        basicStory = self.getBasicStoryText()
+        inputs = self.getInputsEntered()
+        for x in inputs:
+            basicStory = basicStory.format(x)
+        return basicStory
+
+
+dayAtDisneyText = "Today, I went to the Disney World with my best friend. I saw a {} {} " \
+                  "in a {} show at the Magic Kingdom and ate {} for dinner. The next day I ran {}" \
+                  "to meet Mickey Mouse in his {} and that night I gazed at the {} fireworks shooting" \
+                  "from the {}"
+dayAtDisney = Story(["adjective", "noun", "adjective", "noun", "adverb", "noun", "adjective", "noun"], dayAtDisneyText)
+
+# ------------------
+# Yay, this is a thing!
+string = "hello there {}"
+string = string.format('hello')
+print(string)
+# ------------------
 
 
 
@@ -50,6 +75,6 @@ def welcomeMessage(speech) :
     return "Welcome to the Mad Libs Generator! Enter an {} to start your story!".format('hello')
 
 
-print(welcomeMessage())
+print(welcomeMessage('speech'))
 
 
