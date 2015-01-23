3b2b9395ccf7ac1d6b78298f0f54385557b2c2f2
Kathryn Hodge <kahodge@vassar.edu>
2015-01-23 13:41:59 -0600

    Mad Libs are Printing!

--- madLibs.py
+++ madLibs.py
@@ -2,28 +2,29 @@ import sys
 import os
 
 # MAD LIBS OBJECTIVES
-# The user inputs an adjective, noun, adverb, or verb, depending on the what the program asks for.
-# After entering 8 words, the program prints out a story with the adjectives, nouns, adverbs, and
-# verbs the user inputted.
+# The user inputs an adjective, noun, or adverb depending on the what the program asks for.
+# After entering a certain number of words, the program prints out a story with the adjectives,
+# nouns, and adverbs the user inputted.
 
-# The program should also check to make sure the user inputs the correct part of speech according to
-# what the program is asking for.
+# The program should also check to make sure the user inputs the correct part of speech according
+# to what the program is asking for.
 
-# If the user does not input anything (aka just presses enter), then the program will come up with a random
-# adjective, noun, etc to put in the space.
+# If the user does not input anything (aka just presses enter), then the program will
+# come up with a random adjective, noun, etc to put in the space.
 
 # Add a feature where the user could create a story?
 
-class Story :
+
+class MadLib :
     __orderOfParts = [""]
     __text = ""
     __inputsEntered = []
     __index = 0
 
-#Initalizers | Getters & Setters
+    #Initalizers | Getters & Setters
     def __init__ (self, orderOfParts, story):
         self.__orderOfParts = orderOfParts
-        self.__story = story
+        self.__text = story
 
     def getNextOrderOfParts(self):
         oldIndex = self.getIndex()
@@ -50,7 +51,8 @@ class Story :
             return self.__inputsEntered[index]
 
     def setNextInput(self, user):
-        self.__inputsEntered.append(user)
+        # The range takes out /n
+        self.__inputsEntered.append(user[:-1])
 
     def getIndex(self):
         return self.__index
@@ -69,27 +71,32 @@ class Story :
     def putStoryTogether(self):
         basicStory = self.getBasicStoryText()
         inputs = self.getInputsEntered()
-        for x in inputs:
-            basicStory = basicStory.format(x)
-        return basicStory
+        # TO DO: figure out a cleaner way to do this
+        finishedStory = basicStory.format(inputs[0],inputs[1], inputs[2],
+                                       inputs[3], inputs[4], inputs[5],
+                                       inputs[6],inputs[7])
+        return finishedStory
+
 
 # ------------------
 # Yay, this is a thing!
-string = "hello there {}"
-string = string.format('hello')
-print(string)
+# string = "hello there {}"
+# string = string.format('hello')
+# print(string)
 # ------------------
 
 
-dayAtDisneyText = "Today, I went to the Disney World with my best friend. I saw a {} {} " \
-                  "in a {} show at the Magic Kingdom and ate {} for dinner. The next day I ran {}" \
-                  "to meet Mickey Mouse in his {} and that night I gazed at the {} fireworks shooting" \
-                  "from the {}"
-dayAtDisney = Story(["adjective", "noun", "adjective", "noun", "adverb", "noun", "adjective", "noun"],
+# Example of a Mad Lib
+# TO DO: Create a data base with many Mad Libs and have the program choose one at random
+dayAtDisneyText = "\nToday, I went to the Disney World with my best friend. I saw a {} {} \n" \
+                  "in a {} show at the Magic Kingdom and ate {} for dinner. The next day \nI ran {}" \
+                  " to meet Mickey Mouse in his {} and that night I gazed at \nthe {} fireworks shooting" \
+                  " from the {}"
+dayAtDisney = MadLib(["adjective", "noun", "adjective", "noun", "adverb", "noun", "adjective", "noun"],
                     dayAtDisneyText)
 
-# Messages
 
+# Messages
 def welcomeMessage(speech):
     return "Welcome to the Mad Libs Generator! Enter an {} to start your story!".format\
         (speech)
@@ -101,6 +108,7 @@ def nextMessage(speech):
 def endMessage():
     return "Congratulations! You finished your story!"
 
+
 #RUNNING THIS:
 firstPartOfSpeech = dayAtDisney.getNextOrderOfParts()
 print(welcomeMessage(firstPartOfSpeech))
@@ -109,14 +117,16 @@ inputOne = sys.stdin.readline()
 dayAtDisney.setNextInput(inputOne)
 
 i = len(dayAtDisney.getAllOrderOfParts()) - 1
-
 for x in range(0, i):
     nextPartOfSpeech = dayAtDisney.getNextOrderOfParts()
     print(nextMessage(nextPartOfSpeech))
     nextInput = sys.stdin.readline()
     #Check to make sure it matches up to the correct part of speech
     dayAtDisney.setNextInput(nextInput)
+   # print(dayAtDisney.getAllOrderOfParts())
+   # print(dayAtDisney.getInputsEntered())
 
+print('\n')
 print(endMessage())
 print(dayAtDisney.stringStory())
 
